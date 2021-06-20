02253f475c5275f18616aa1fc69adab938d68e59
amankadam <aman.kadam.268@gmail.com>
2021-06-20 14:32:38 +0530

    code format

--- store/keyboard/index.tsx
+++ store/keyboard/index.tsx
@@ -1,12 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { createSlice } from '@reduxjs/toolkit';
-import { SPECIAL_KEYS } from '../../constants';
-// interface Ikey {
-//   inputValue: string;
-//   capsLock: boolean;
-//   LShift: boolean;
-//   RShift: boolean;
-// }
 const keyBoardSlice = createSlice({
   name: 'keyboard',
   initialState: {
